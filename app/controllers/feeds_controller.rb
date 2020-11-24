945ce0302a885b2ebd73d57bd24d407a5ff1fa5c
Edem Dorfe <dayonafi@gmail.com>
2020-11-24 09:14:32 +0900

    edit style, removed comments

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,18 +1,15 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
 
-  # GET /feeds
-  # GET /feeds.json
+
   def index
     @feeds = Feed.all
   end
 
-  # GET /feeds/1
-  # GET /feeds/1.json
+
   def show
   end
 
-  # GET /feeds/new
   def new
     if params[:back]
       @feed = Feed.new(feed_params)
@@ -21,12 +18,9 @@ class FeedsController < ApplicationController
     end
   end
 
-  # GET /feeds/1/edit
   def edit
   end
 
-  # POST /feeds
-  # POST /feeds.json
   def create
     @feed = Feed.new(feed_params)
     @feed.user_id = current_user.id
@@ -42,8 +36,7 @@ class FeedsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /feeds/1
-  # PATCH/PUT /feeds/1.json
+
   def update
     respond_to do |format|
       if @feed.update(feed_params)
@@ -56,8 +49,7 @@ class FeedsController < ApplicationController
     end
   end
 
-  # DELETE /feeds/1
-  # DELETE /feeds/1.json
+
   def destroy
     @feed.destroy
     respond_to do |format|
@@ -74,12 +66,10 @@ class FeedsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_feed
       @feed = Feed.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def feed_params
       params.require(:feed).permit(:image, :image_cache, :post, :user_id, :id)
     end
