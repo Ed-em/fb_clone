d008956ad95ab5cef2c64d7d26efc01321b412d8
Edem Dorfe <dayonafi@gmail.com>
2020-11-24 12:31:16 +0900

    Please confirm

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,11 +1,9 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
 
-
   def index
     @feeds = Feed.all
   end
-
 
   def show
   end
